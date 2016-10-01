053e7aebab514aadebc761c3ba807fcee09e66b8
Frank Mitchell <me@frankmitchell.org>
2016-10-01 08:09:33 -0400

    Basic REPL with 'help' and 'quit' commands.

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::io::{self, Write};
+
+fn main() {
+    println!("Welcome to Chifir!");
+    println!("");
+    println!("Type 'help' to get started.");
+
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut command = String::new();
+
+        io::stdin().read_line(&mut command).expect("Failed to read command");
+
+        match command.trim() {
+            "help" => println!("help, quit"),
+            "quit" => break,
+            _ => continue
+        }
+    }
+}
