dd8fd4f05a606b7e390e61978749d5470d901421
Frank Mitchell <me@frankmitchell.org>
2016-10-01 12:07:56 -0400

    Test that opcdoe 2 works.

--- src/machine.rs
+++ src/machine.rs
@@ -25,12 +25,21 @@ impl Machine {
         self.memory = data;
     }
 
-    pub fn exec(&mut self, opcode: u32, a: u32) {
+    pub fn exec(&mut self, opcode: u32, a: u32, b: u32) {
         match opcode {
             // PC <- M[A]
             1 => {
                 self.counter = self.memory[a as usize];
             },
+
+            // If M[B] = 0, then PC <- M[A]
+            2 => {
+                if 0 == self.memory[b as usize] {
+                  self.counter = self.memory[a as usize];
+                }
+            }
+
+            // Unknown opcode
             _ => {
             },
         }
@@ -56,7 +65,27 @@ mod tests {
         assert_eq!(0, m.loc());
 
         m.load(vec![0, 2]);
-        m.exec(1, 1);
+        m.exec(1, 1, 0);
         assert_eq!(2, m.loc());
+    }
+
+    #[test]
+    fn it_runs_opcode_2_if() {
+        let mut m = Machine::new();
+        assert_eq!(0, m.loc());
+
+        m.load(vec![0, 2]);
+        m.exec(2, 1, 0);
+        assert_eq!(2, m.loc());
+    }
+
+    #[test]
+    fn it_runs_opcode_2_else() {
+        let mut m = Machine::new();
+        assert_eq!(0, m.loc());
+
+        m.load(vec![1, 2]);
+        m.exec(2, 1, 0);
+        assert_eq!(0, m.loc());
     }
 }
