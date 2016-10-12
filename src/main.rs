f99cba7f5f2f5dfe2a56f137d5c04f5c6f7bccbd
Frank Mitchell <me@frankmitchell.org>
2016-10-12 17:38:23 -0400

    Simpler "Wait for ctrl-c" loop.

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,10 @@ fn main() {
 
     let mut vm = chifir::machine::Machine {
         memory: vec![
-            14,0,0,1,   // 00 - Clear the screen
-            15,6,0,3,   // 04 - Read key press and store it in $6
-            8,3,7,6,    // 08 - $3 <- $7 - $6
-            2,15,3,20,  // 12 - If $3 = 0, then PC <- 20
-            1,19,0,4,   // 16 - Else PC <- 4
-            0,0,0,0,    // 20 - Break
+            0xf, 0x2, 0x0, 0x3, // 00 - Read key press and store it in $2
+            0x8, 0x2, 0x2, 0x3, // 04 - $2 <- $2 - $3
+            0x2, 0xb, 0x2, 0xf, // 08 - If $2 = 0, then PC <- 15
+            0x1, 0xf, 0x0, 0x0, // 12 - Else PC <- 0
         ],
         counter: 0,
         output: stdout,
