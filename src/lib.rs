31621bda2fb3658c7a64c30836f372137b5dece9
Frank Mitchell <me@frankmitchell.org>
2016-10-17 22:30:44 -0400

    Link to the original paper describing the Chifir virtual computer.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! Chifir is a virtual computer based on the Tien Nguyen and Alan Kay's paper,
+//! ["The Cuneiform Tablets of 2015"][paper].
+//!
+//!
+//! [paper]: http://www.vpri.org/pdf/tr2015004_cuneiform.pdf "Tien Nguyen and Alan Kay (Viewpoints Research Insitute): The Cuneiform Tablets of 2015"
 extern crate termion;
 
 pub mod computer;
